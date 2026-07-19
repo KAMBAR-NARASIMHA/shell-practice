0c5f001281f8c39b0261a7fd0b39d5b0671a9a66
kambar narasimha <kambarnarasimha4@gmail.com>
2026-07-19 13:06:22 +0530

    04-hello.sh

--- 04-hello.sh
+++ 04-hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Please enter your PIN_NUMBER"
-read PIN_NUMBER # here the PIN_NUMBER is available
+read -s PIN_NUMBER # here the PIN_NUMBER is available
 echo "PIN enter is: $PIN_NUMBER"
